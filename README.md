ebb60532b03f023abfd8f3d0aa56a4cde359070c
psc3245 <psc3.omaha@gmail.com>
2025-01-03 13:44:19 -0600

    Arb Optimizer done for now

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # cbb-money
+
+## (1/3/24) Changes:
+arb_optimizer.py is done for now. 
+- May be necessary is converting it into a script that accepts arguments for odds rather than user input
+- May be necessary to alter how it prints the odds as well
+Going to work on scraping from betting sites next. After that, we set up DB to put them in.
+
+
+
+## Arb Betting Notes / Ideas
+
+- VPN or spoof IP to avoid sports book IP ban
+- use AWS to host scripts and dbs
+
+Steps:
+1. Run script on time interval to scrape lines
+2. 
+
+Found someone elses API for this, pretty sure API key costs money tho
+https://rapidapi.com/odds-papi-odds-papi-default/api/odds-api1/playground/apiendpoint_f219be79-51fe-4f39-a902-38a62d1dd4cf
--- arb_optimizer.py
+++ arb_optimizer.py
@@ -1,5 +1,3 @@
-# Goal: create a small program that takes in two sets of odds and finds the most profitable setup for betting in increments of 50 cents
-
 def americanToMultiplier(odds) -> float:
     if (odds[0] == "+"):
         return 1.0 + (float(odds[1:]) / 100)
@@ -21,42 +19,60 @@ def calculateHold(bet, oddsA, oddsB) -> float:
     impliedProbB = americanToImpliedProb(oddsB)
 
     impliedProbSum = impliedProbA + impliedProbB
-    print("\n")
-    print("Probability Summary:")
-    print(f"Implied probability A: {impliedProbA}")
-    print(f"Implied probability B: {impliedProbB}")
-    print(f"Implied probability total: {impliedProbSum}")
-    print("\n")
 
+    # print(f"Probability Summary: \n   Implied probability A: {impliedProbA} \n   Implied probability B: {impliedProbB} \n   Implied probability total: {impliedProbSum}")
 
     if (impliedProbSum < 1.0):
-        print(f"Odds {oddsA} and {oddsB} is an arbitrage opportunity. \n")
 
         toBetA = impliedProbA / impliedProbSum * bet
+        if (toBetA - int(toBetA) < 0.5): 
+            toBetA_rounded = int(toBetA)
+        else:
+            toBetA_rounded = int(toBetA) + 1
+
         profitA = toBetA * multiplerA - bet
-        print(f"A ${toBetA:.2f} bet on {oddsA} will yield {profitA:.2f}")
+        profitA_rounded = toBetA_rounded * multiplerA - bet
 
         toBetB = impliedProbB / impliedProbSum * bet
-        profitB = toBetB * multiplerB - bet
-        print(f"A ${toBetB:.2f} bet on {oddsB} will yield {profitB:.2f}")
-        print("\n")
+        if (toBetB - int(toBetB) < 0.5): 
+            toBetB_rounded = int(toBetB)
+        else:
+            toBetB_rounded = int(toBetB) + 1
 
-        hold = (profitA + profitB) / (bet * 2) * 100
-        
-        if (profitA == profitB): print(f"Calculated {hold:.2f}% hold on this opportunity.")
-        else: print(f"Calculated {hold:.2f}% average hold on this opportunity.")
+        profitB = toBetB * multiplerB - bet
+        profitB_rounded = toBetB_rounded * multiplerB - bet
+
+        print("Rounded Betting:")
+        print(f" - A rounded ${toBetA_rounded:.2f} bet on {oddsA} will yield {profitA_rounded:.2f}")
+        print(f" - A rounded ${toBetB_rounded:.2f} bet on {oddsB} will yield {profitB_rounded:.2f}")
+        print(f"Calculated {((profitA_rounded + profitB_rounded) / (bet * 2) * 100):.2f}% average hold on this opportunity. \n")
+
+        print("True Arbitrage Betting:")
+        print(f" - A ${toBetA:.2f} bet on {oddsA} will yield {profitA:.2f}")
+        print(f" - A ${toBetB:.2f} bet on {oddsB} will yield {profitB:.2f}")
+        print(f"Calculated {((profitA + profitB) / (bet * 2) * 100):.2f}% hold on this opportunity.")
+
     else:
         print(f"Odds {oddsA} and {oddsB} is NOT an arbitrage opportunity.")
 
 
 keepgoing = True
 
-if(keepgoing): #while (keepgoing):
+while (keepgoing):
     bet = int(input("How Much To Bet: "))
     oddsA = input("Site A Odds: ")
     oddsB = input("Site B Odds: ")
 
+    print()
+    print("Results: ")
+    print("----------------------------")
+
     calculateHold(bet, oddsA, oddsB)
+
+    print("----------------------------")
+
+    keepgoing = (input("Do another bet? (y/n): ") == "y")
+    print()
 
     
         
